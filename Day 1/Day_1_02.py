69ab9263e68123add1cf93b1ce81c8a4afcaf642
Tibin Abraham <tibinbm@gmail.com>
2024-09-11 12:21:38 +0530

    Update Day_1_02.py

--- Day 1/Day_1_02.py
+++ Day 1/Day_1_02.py
@@ -2,6 +2,7 @@ import cv2
 
 capture = cv2.VideoCapture(0);
 
+'''
 while(True):
     ret, frame = capture.read()
 
@@ -11,5 +12,17 @@ while(True):
         break
 capture.release()
 cv2.destroyAllWindows()
+'''
+import cv2
+ret, frame = capture.read()
+
+if ret:  # Check if the frame was captured successfully
+    cv2.imshow('window', frame)
+    cv2.waitKey(0)  # Wait indefinitely until a key is pressed
+
+# Release the capture and close the window
+capture.release()
+cv2.destroyAllWindows()
+
 
 
